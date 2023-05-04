28f2ef04e31b40698c291cfbe63bbc61be56f1c2
sara-rioseco <sara.rioseco@gmail.com>
2023-05-04 13:44:12 -0400

    Updates

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-fetch("./data/pokemon/pokemon.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (pokemon) {
-    for (let i = 0; i < pokemon.pokemon.length; i++) {
-      const src =
-        "https://www.serebii.net/pokemongo/pokemon/" +
-        pokemon.pokemon[i].num +
-        ".png";
-      const img = document.createElement("img");
-      img.src = src;
-      document.getElementById("data-output").appendChild(img);
-      const pokebox = document.createElement("div");
-      pokebox.innerHTML = document.getElementById("data-output").innerHTML +=
-        "<br />" +
-        pokemon.pokemon[i].num +
-        "<br />" +
-        pokemon.pokemon[i].name +
-        "<br />" +
-        pokemon.pokemon[i].generation.num +
-        "<br />" +
-        pokemon.pokemon[i].generation.name +
-        "<br />";
-    }
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+import data from "./data/pokemon/pokemon.js";
+
+function showPokemon() {
+  const pokeContainer = document.querySelector(".flex-container");
+  for (let i = 0; i < data.pokemon.length; i++) {
+    const createPokebox = document.createElement("div");
+    createPokebox.className = "pokeDiv";
+    createPokebox.innerHTML += data.pokemon[i].num
+    createPokebox.innerHTML += "<br>"
+    createPokebox.innerHTML += `<img src= "${data.pokemon[i].img}" alt= "pokeImg${data.pokemon[i].name}" class="image" id="${data.pokemon[i].id}">`;
+    createPokebox.innerHTML += "<br>"
+    createPokebox.innerHTML += data.pokemon[i].name
+    pokeContainer.insertAdjacentElement("beforeend", createPokebox)
+  }
+}
+showPokemon();
+
+/*    const src =
+      "https://www.serebii.net/pokemongo/pokemon/" +
+      pokemon[i].num +
+      ".png";
+    const img = document.createElement("img");
+    img.src = src;
+    document.getElementById("data-output").appendChild(img);
+    pokebox.innerHTML = document.getElementById("data-output").innerHTML +=
+      "<br />" +
+      pokemon[i].num +
+      "<br />" +
+      pokemon[i].name +
+      "<br />" +
+      pokemon[i].generation.num +
+      "<br />" +
+      pokemon[i].generation.name +
+      "<br />";
+  }
+  return showPokemon();
+};*/
