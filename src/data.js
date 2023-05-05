a0b06f7bf52cc27b036f953022606a8c8d422f03
sara-rioseco <sara.rioseco@gmail.com>
2023-05-05 14:17:55 -0400

    avance funcion busqueda

--- src/data.js
+++ src/data.js
@@ -1,15 +1,35 @@
-export const pokeSearch = function() {
-  const input = document.getElementById("searchbar");
-  const filter = input.value.toUpperCase();
-  const pokeContainer = document.getElementById("flex-container");
-  const pokeDiv = pokeContainer.getElementsByTagName("div");
-  for (let i=0; i<pokeDiv.length; i++) {
-    const boxDiv = pokeDiv[i].getElementsByClassName("pokeDiv")[0];
-    const txtValue = boxDiv.textContent || boxDiv.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      pokeDiv[i].style.display = "";
-    } else {
-      pokeDiv[i].style.display = "none";
+import data from "./data/pokemon/pokemon.js";
+const dataFunctions = {
+  showPokemon: function () {
+    const pokeContainer = document.querySelector(".flex-container");
+    for (let i = 0; i < data.pokemon.length; i++) {
+      const createPokebox = document.createElement("div");
+      const pokeName = data.pokemon[i].name;
+      const pokeNum = data.pokemon[i].num;
+      createPokebox.className = "pokeDiv";
+      createPokebox.id = "pokeDiv" + [i];
+      createPokebox.innerHTML += pokeNum.toString();
+      createPokebox.innerHTML += "<br>";
+      createPokebox.innerHTML += `<img src= "${data.pokemon[i].img}" alt= "pokeImg${data.pokemon[i].name}" class="image" id="${data.pokemon[i].id}">`;
+      createPokebox.innerHTML += "<br>";
+      createPokebox.innerHTML += pokeName.toUpperCase();
+      pokeContainer.insertAdjacentElement("beforeend", createPokebox);
+      /*console.log(createPokebox);*/
     }
-  }
+  },
+  searchPokemon: function () {
+    let input = document.getElementById("searchbar").value;
+    input = input.toLowerCase();
+    const pokeDiv = [];
+    for (let i = 0; i < pokeDiv.length; i++) {
+      pokeDiv.push(document.getElementsByClassName("pokeDiv"));
+      if (!pokeDiv[i].innerHTML.toLowerCase().includes(input,0)) {
+        pokeDiv[i].style.display = "none";
+      } else {
+        pokeDiv[i].style.display = "flex";
+      }    
+      return pokeDiv
+    }
+  },
 };
+export default dataFunctions;
--- src/main.js
+++ src/main.js
@@ -1,38 +1,5 @@
-/*import { pokeSearch } from "./data.js";*/
-import data from "./data/pokemon/pokemon.js";
+import dataFunctions from "./data.js";
 
-function showPokemon() {
-  const pokeContainer = document.querySelector(".flex-container");
-  for (let i = 0; i < data.pokemon.length; i++) {
-    const createPokebox = document.createElement("div");
-    const pokeName = data.pokemon[i].name
-    const pokeNum = data.pokemon[i].num
-    createPokebox.className = "pokeDiv";
-    createPokebox.id = "pokeDiv" + [i]
-    createPokebox.innerHTML += pokeNum.toString()
-    createPokebox.innerHTML += "<br>"
-    createPokebox.innerHTML += `<img src= "${data.pokemon[i].img}" alt= "pokeImg${data.pokemon[i].name}" class="image" id="${data.pokemon[i].id}">`;
-    createPokebox.innerHTML += "<br>"
-    createPokebox.innerHTML += pokeName.toUpperCase()
-    pokeContainer.insertAdjacentElement("beforeend", createPokebox)
-    console.log(createPokebox)
-  }
-}
-showPokemon();
+dataFunctions.showPokemon();
 
-
-function searchPokemon () {
-  const input = document.getElementById("searchbar").value;
-  const filter = input.value.toUpperCase();
-  const pokeContainer = document.getElementById("flex-container");
-  const pokeDiv = pokeContainer.getElementsByClassName("pokeDiv");
-  for (let i=0; i<pokeDiv.length; i++) {
-    const boxDiv = pokeDiv[i].getElementsByClassName("pokeDiv")[0];
-    const txtValue = boxDiv.textContent || boxDiv.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      pokeDiv[i].style.display = "";
-    } else {
-      pokeDiv[i].style.display = "none";
-    }
-  }
-}
+dataFunctions.searchPokemon();
