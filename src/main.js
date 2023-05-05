02849bef0d7fb7bc1fa5d391cb314875b122042b
sara-rioseco <sara.rioseco@gmail.com>
2023-05-05 13:00:03 -0400

    cambios diseño

--- src/main.js
+++ src/main.js
@@ -8,17 +8,20 @@ function showPokemon() {
     const pokeName = data.pokemon[i].name
     const pokeNum = data.pokemon[i].num
     createPokebox.className = "pokeDiv";
+    createPokebox.id = "pokeDiv" + [i]
     createPokebox.innerHTML += pokeNum.toString()
     createPokebox.innerHTML += "<br>"
     createPokebox.innerHTML += `<img src= "${data.pokemon[i].img}" alt= "pokeImg${data.pokemon[i].name}" class="image" id="${data.pokemon[i].id}">`;
     createPokebox.innerHTML += "<br>"
     createPokebox.innerHTML += pokeName.toUpperCase()
     pokeContainer.insertAdjacentElement("beforeend", createPokebox)
+    console.log(createPokebox)
   }
 }
 showPokemon();
 
-function pokeSearch () {
+
+/*function searchPokemon () {
   const input = document.getElementById("searchbar");
   const filter = input.value.toUpperCase();
   const pokeContainer = document.getElementById("flex-container");
@@ -32,5 +35,4 @@ function pokeSearch () {
       pokeDiv[i].style.display = "none";
     }
   }
-}
-
+}*/
