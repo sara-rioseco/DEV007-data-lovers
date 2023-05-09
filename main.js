712ff2f5a87419c64c97e3e416507ad76f0fe02d
sara-rioseco <sara.rioseco@gmail.com>
2023-05-09 15:16:29 -0400

    Updates

--- main.js
+++ main.js
@@ -2,4 +2,25 @@ import dataFunctions from "./data.js";
 
 dataFunctions.showPokemon();
 
-dataFunctions.searchPokemon();
+const pokeContainer = document.querySelector(".flex-container");
+const pokeInput = document.getElementById("searchbar");
+const searchBttn = document.getElementById("searchbutton");
+
+searchBttn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const searchResult = dataFunctions.pokeNames(pokeInput.value.toLowerCase());
+  pokeContainer.innerHTML = ""
+  for (let i = 0; i < searchResult.length; i++) {
+    const createPokebox = document.createElement("li");
+    const pokeName = searchResult[i].name;
+    const pokeNum = searchResult[i].num;
+    createPokebox.className = "pokeLi";
+    createPokebox.id = "pokeLi" + [i];
+    createPokebox.innerHTML += pokeNum.toString();
+    createPokebox.innerHTML += "<br>";
+    createPokebox.innerHTML += `<img src= "${searchResult[i].img}" alt= "pokeImg${searchResult[i].name}" class="image" id="${searchResult[i].id}">`;
+    createPokebox.innerHTML += "<br>";
+    createPokebox.innerHTML += pokeName.toUpperCase();
+    pokeContainer.insertAdjacentElement("beforeend", createPokebox);
+  }
+});
