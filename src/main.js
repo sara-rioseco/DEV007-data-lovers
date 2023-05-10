27145711d19783f6a4f696978b8cee688abc2d25
sara-rioseco <sara.rioseco@gmail.com>
2023-05-09 23:56:53 -0400

    funcion busqueda por nombre, numero y tipo

--- src/main.js
+++ src/main.js
@@ -8,19 +8,71 @@ const searchBttn = document.getElementById("searchbutton");
 
 searchBttn.addEventListener("click", (e) => {
   e.preventDefault();
-  const searchResult = dataFunctions.pokeNames(pokeInput.value.toLowerCase());
-  pokeContainer.innerHTML = ""
-  for (let i = 0; i < searchResult.length; i++) {
-    const createPokebox = document.createElement("li");
-    const pokeName = searchResult[i].name;
-    const pokeNum = searchResult[i].num;
-    createPokebox.className = "pokeLi";
-    createPokebox.id = "pokeLi" + [i];
-    createPokebox.innerHTML += pokeNum.toString();
-    createPokebox.innerHTML += "<br>";
-    createPokebox.innerHTML += `<img src= "${searchResult[i].img}" alt= "pokeImg${searchResult[i].name}" class="image" id="${searchResult[i].id}">`;
-    createPokebox.innerHTML += "<br>";
-    createPokebox.innerHTML += pokeName.toUpperCase();
-    pokeContainer.insertAdjacentElement("beforeend", createPokebox);
+  const searchNameResult = dataFunctions.searchName(
+    pokeInput.value.toLowerCase()
+  );
+  const searchNumberResult = dataFunctions.searchNumber(pokeInput.value);
+  const searchTypeResult = dataFunctions.searchType(
+    pokeInput.value.toLowerCase()
+  );
+  pokeContainer.innerHTML = "";
+  if (pokeInput.value === "") {
+    const createMessage1 = document.createElement("div");
+    createMessage1.id = "noinput";
+    createMessage1.className = "messages";
+    createMessage1.innerHTML +=
+      "<h2> Por favor, ingresa el nombre, número o tipo de Pokemon para buscar.</h2>";
+    createMessage1.innerHTML += "<img src=./img/HappyPikachu.png>";
+    pokeContainer.insertAdjacentElement("beforeend", createMessage1);
+  } else if (
+    (searchNameResult.toString() === "" && searchNumberResult.toString() === "" && searchTypeResult.toString() === "")
+  ) {
+    const createMessage2 = document.createElement("div");
+    createMessage2.id = "notfoundmessage";
+    createMessage2.className = "messages";
+    createMessage2.innerHTML +=
+      "<h2> Lo sentimos, tu búsqueda no ha dado resultados.</h2>";
+    createMessage2.innerHTML += "<img src=./img/SadPikachu.png>";
+    pokeContainer.insertAdjacentElement("beforeend", createMessage2);
+  } else {
+    for (let i = 0; i < searchNameResult.length; i++) {
+      const createPokebox = document.createElement("li");
+      const pokeName = searchNameResult[i].name;
+      const pokeNum = searchNameResult[i].num;
+      createPokebox.className = "pokeLi";
+      createPokebox.id = "pokeLi" + [i];
+      createPokebox.innerHTML += pokeNum.toString();
+      createPokebox.innerHTML += "<br>";
+      createPokebox.innerHTML += `<img src= "${searchNameResult[i].img}" alt= "pokeImg${searchNameResult[i].name}" class="image" id="${searchNameResult[i].id}">`;
+      createPokebox.innerHTML += "<br>";
+      createPokebox.innerHTML += pokeName.toUpperCase();
+      pokeContainer.insertAdjacentElement("beforeend", createPokebox);
+    }
+    for (let i = 0; i < searchNumberResult.length; i++) {
+      const createPokebox = document.createElement("li");
+      const pokeName = searchNumberResult[i].name;
+      const pokeNum = searchNumberResult[i].num;
+      createPokebox.className = "pokeLi";
+      createPokebox.id = "pokeLi" + [i];
+      createPokebox.innerHTML += pokeNum.toString();
+      createPokebox.innerHTML += "<br>";
+      createPokebox.innerHTML += `<img src= "${searchNumberResult[i].img}" alt= "pokeImg${searchNumberResult[i].name}" class="image" id="${searchNumberResult[i].id}">`;
+      createPokebox.innerHTML += "<br>";
+      createPokebox.innerHTML += pokeName.toUpperCase();
+      pokeContainer.insertAdjacentElement("beforeend", createPokebox);
+    }
+    for (let i = 0; i < searchTypeResult.length; i++) {
+      const createPokebox = document.createElement("li");
+      const pokeName = searchTypeResult[i].name;
+      const pokeNum = searchTypeResult[i].num;
+      createPokebox.className = "pokeLi";
+      createPokebox.id = "pokeLi" + [i];
+      createPokebox.innerHTML += pokeNum.toString();
+      createPokebox.innerHTML += "<br>";
+      createPokebox.innerHTML += `<img src= "${searchTypeResult[i].img}" alt= "pokeImg${searchTypeResult[i].name}" class="image" id="${searchTypeResult[i].id}">`;
+      createPokebox.innerHTML += "<br>";
+      createPokebox.innerHTML += pokeName.toUpperCase();
+      pokeContainer.insertAdjacentElement("beforeend", createPokebox);
+    }
   }
 });
